f0ff3d778117db11ae58490e5a74b3f22a81a1c6
NatalliaMaskalenko <MoskNatulya@gmail.com>
2022-02-15 18:53:03 +0100

    Первая буква имени

--- src/components/Header/UserProfile/UserProfile.jsx
+++ src/components/Header/UserProfile/UserProfile.jsx
@@ -5,10 +5,22 @@ import { AvatarLetter, UserName, ProfileWrapper } from "./UserProfile.styled";
 function UserProfile(isAuth) {
   const userEmail = useSelector(getUserEmail)
   const userName = (userEmail) => {
-    const index = userEmail.indexOf("@");
-   return userEmail.slice(0, index)
+    if (userEmail) {
+      const index = userEmail.indexOf("@");
+    return userEmail.slice(0, index)
+    }
+    return "Guest";
+  };
 
-  }
+  const userFirstLetter = (userEmail) => {
+    if (userEmail) {
+       return userEmail.slice(0, 1);
+    }
+    return "G";
+  };
+
+  // 
+
   return (
     
     <ProfileWrapper>
@@ -16,7 +28,7 @@ function UserProfile(isAuth) {
                 ( <img src="/" alt="Avatar" />) 
                 :  
                 (< className={s.userFirstLetter}>{UserFirstLetter}</p>)} */}
-      <AvatarLetter>U</AvatarLetter>
+      <AvatarLetter>{userFirstLetter(userEmail)}</AvatarLetter>
       <UserName> {userName(userEmail)} </UserName>
     </ProfileWrapper>
   );

f68b5cd87e4e36806ac252493b3ca6030d44b963
Andrii Konovalov <andy.smokk@gmail.com>
2022-02-16 22:31:47 +0100

    add lazy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { toast } from "react-toastify";
 import { userIsAuth } from "./redux/selectors";
-import {
-  MainPage,
-  PageNotFound,
-  ReportPage,
-  LoginPage,
-  GoogleAuthPage,
-  MailConfirmationPage,
-} from "./pages";
 import { PublicRoute, PrivateRoute } from "./routes";
 import { Layout } from "./layout";
-// ???????
 import { IncomeModule } from "./components/IncomeModule";
-// import { Background } from "./components/Background";
 import { FormComponent } from "./components/FormComponent";
 import { authNotifyStatus } from "./redux/selectors";
+import { LoaderReport } from "./components/Loader";
+
+const LoginPage = lazy(() =>
+  import("./pages/LoginPage" /* webpackChunkName: "login_page" */),
+);
+const MainPage = lazy(() =>
+  import("./pages/MainPage" /* webpackChunkName: "main_page" */),
+);
+const ReportPage = lazy(() =>
+  import("./pages/ReportPage" /* webpackChunkName: "report_page" */),
+);
+const GoogleAuthPage = lazy(() =>
+  import("./pages/GoogleAuthPage" /* webpackChunkName: "google_auth_page" */),
+);
+const PageNotFound = lazy(() =>
+  import("./pages/PageNotFound" /* webpackChunkName: "not_found_page" */),
+);
+const MailConfirmationPage = lazy(() =>
+  import(
+    "./pages/MailConfirmationPage" /* webpackChunkName: "mail_confirmation_page" */
+  ),
+);
 
 function App() {
   const notify = useSelector(authNotifyStatus);
@@ -42,50 +54,39 @@ function App() {
   return (
     <>
       <Layout isAuth={isAuth}>
-        {/* <nav>
-          <button>
-            <Link to="login">login</Link>
-          </button>
-          <button>
-            <Link to="/">Main</Link>
-          </button>
-          <button>
-            <Link to="report">Report</Link>
-          </button>
-        </nav> */}
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              <PublicRoute redirectTo="/">
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/*"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <MainPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/report/*"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <ReportPage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/googleAuth" element={<GoogleAuthPage />} />
-          <Route path="/mailConfirm" element={<MailConfirmationPage />} />
-          {/* ?????????? */}
-          <Route path="/expense" element={<FormComponent />} />
-          <Route path="/income" element={<IncomeModule />} />
-          {/* ??????? */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<LoaderReport />}>
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                <PublicRoute redirectTo="/">
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/*"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <MainPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/report/*"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <ReportPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/googleAuth" element={<GoogleAuthPage />} />
+            <Route path="/mailConfirm" element={<MailConfirmationPage />} />
+            <Route path="/expense" element={<FormComponent />} />
+            <Route path="/income" element={<IncomeModule />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </>
   );
